87f3ee1252218c82bfdc46c0d574688f3971faad
33585 <33585@FENGSHILV>
2022-04-20 09:03:31 +0800

    添加项目文件。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Personnel_information_registration_form.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
